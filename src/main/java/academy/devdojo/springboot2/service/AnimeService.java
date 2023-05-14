935a76a076c1c0e654f44d677dde2a9e30a740ec
= <brunow9@outlook.com>
2023-05-14 12:44:03 -0300

    Aula método POST ok

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -5,11 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class AnimeService {
 
-    List<Anime> animes = List.of(new Anime(1L,"Boku no Hero"), new Anime(2L,"One Piece"));
+    static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L,"Boku no Hero"), new Anime(2L,"One Piece")));
+    }
     // private final AnimeRepository animeRepository
     public List<Anime> listAll() {
         return animes;
@@ -20,5 +27,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000));
+        animes.add(anime);
+        return anime;
     }
 }
